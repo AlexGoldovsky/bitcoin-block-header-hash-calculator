9dcd98a32449505e14fcffbb0c7986b417602de9
Alex Goldovsky <alex.lisey@gmail.com>
2018-01-25 19:55:06 +0200

    introduce Clients dir

--- BitBlockHeaderHash/BitBlockHeaderHash.py
+++ BitBlockHeaderHash/BitBlockHeaderHash.py
@@ -14,35 +14,31 @@ class BitBlockHeaderHash:
         self.bits = bits
         self.nonce = nonce
 
-    def __hexToLE(self, hexa):
+    def __hexToLEBin(self, hexstr):
         '''
-        Changes the byte order of a hexadecimal number to little-endian
-        Can't use pack here due to the size of the number
+        Converts a hexadecimal string to a binary little-endian byte order
         '''
-        bin_of_hexa = unhexlify(hexa)
-        return bin_of_hexa[::-1]
+        return unhexlify(hexstr)[::-1]
 
-    def __intToHexLE(self, n):
+    def __intToLEBin(self, n):
         '''
-        Converts an integer to a 4 byte hexadecimal in little-endian byte order
+        Converts an integer to a binary little-endian byte order
         '''
         return pack("<I", n)
-        # check n.to_bytes(4, byteorder = 'little') for python > 3.2
 
     def get_header_hex(self):
-        version = self.__intToHexLE(self.version)
-        previousblockhash = self.__hexToLE(self.pbh)
-        merkleroot = self.__hexToLE(self.merkle_root)
-        time = self.__intToHexLE(self.time)
-        bits = self.__intToHexLE(self.bits)
-        nonce = self.__intToHexLE(self.nonce)
+        version = self.__intToLEBin(self.version)
+        previousblockhash = self.__hexToLEBin(self.pbh)
+        merkleroot = self.__hexToLEBin(self.merkle_root)
+        time = self.__intToLEBin(self.time)
+        bits = self.__intToLEBin(self.bits)
+        nonce = self.__intToLEBin(self.nonce)
 
         return "".join([version, previousblockhash, merkleroot, time, bits,
                        nonce])
 
     def calculate_hash(self):
-        header_hex = self.get_header_hex()
-        #header_bin = unhexlify(header_hex)
-        header_hash = sha256(sha256(header_hex).digest()).digest()
+        header_bin = self.get_header_hex()
+        header_hash = sha256(sha256(header_bin).digest()).digest()
         return header_hash[::-1].encode('hex_codec')
 
